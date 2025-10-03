c945ddfbaa43a75ccd7edf919377cb62f38f0736
garrett <garrett@infopogo.com>
2025-10-02 17:48:35 -0700

    XML: Add lv_label set_recolor

--- src/others/xml/parsers/lv_xml_label_parser.c
+++ src/others/xml/parsers/lv_xml_label_parser.c
@@ -58,6 +58,7 @@ void lv_xml_label_apply(lv_xml_parser_state_t * state, const char ** attrs)
 
         if(lv_streq("text", name)) lv_label_set_text(item, value);
         if(lv_streq("long_mode", name)) lv_label_set_long_mode(item, long_mode_text_to_enum_value(value));
+        if(lv_streq("set_recolor", name)) lv_label_set_recolor(item, lv_xml_parse_bool(value));
         if(lv_streq("bind_text", name)) {
             char buf[256];
             lv_strncpy(buf, value, sizeof(buf));
